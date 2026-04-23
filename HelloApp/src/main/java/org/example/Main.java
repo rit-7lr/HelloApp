aa9f4fbc895b64a9feee763a6ac5de89e0aabc9b
rit-7lr <ritvikb07@gmail.com>
2026-04-23 08:54:52 +0530

    Display 'Hello' with Multiple Command-Line Arguments or Default Message

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main{
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length > 0) {
-            name = args[0];
         }
-
-        System.out.println("Hello, " + name + "!");
     }
-}
+
