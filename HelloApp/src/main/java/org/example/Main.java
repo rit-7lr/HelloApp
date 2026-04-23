6edd5b24465f9a98ff7e7373782182264cad1c77
rit-7lr <ritvikb07@gmail.com>
2026-04-23 09:11:02 +0530

    Display 'Hello' with Multiple Command-Line Arguments using String.join() Method or Default Message

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -2,21 +2,12 @@ package org.example;
 
 public class Main{
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
+        String names = "World";
 
-            String finalNames = "";
-            if (nameBuilder.length() > 0) {
-                // Remove the last ", " (which is 2 characters long)
-                finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-            }
-
-            System.out.println("Hello, " + finalNames + "!");
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
